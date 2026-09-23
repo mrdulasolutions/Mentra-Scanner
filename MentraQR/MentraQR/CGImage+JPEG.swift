3d336752d955ce243115da344e8e9a0932d310d2
M.R. Dula <matt@mrdula.solutions>
2026-09-22 22:37:45 -0400

    Fix capture JPEG display orientation mapping.
    Map EXIF kCGImagePropertyOrientation to UIImage.Orientation instead of using EXIF values as enum rawValues, so saved stream JPEGs (EXIF 1) are not flipped when Captures applies normalizedUpOrientation.

--- MentraQR/MentraQR/CGImage+JPEG.swift
+++ MentraQR/MentraQR/CGImage+JPEG.swift
@@ -20,9 +20,23 @@ enum CGImageJPEG {
               let cgImage = CGImageSourceCreateImageAtIndex(source, 0, fullDecodeOptions as CFDictionary)
         else { return nil }
         let props = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [CFString: Any]
-        let orientationRaw = (props?[kCGImagePropertyOrientation] as? NSNumber)?.intValue ?? 1
-        let orientation = UIImage.Orientation(rawValue: orientationRaw) ?? .up
+        let exifOrientation = (props?[kCGImagePropertyOrientation] as? NSNumber)?.intValue ?? 1
+        let orientation = uiImageOrientation(fromExifOrientation: exifOrientation)
         return UIImage(cgImage: cgImage, scale: 1, orientation: orientation)
+    }
+
+    /// Maps TIFF/EXIF `kCGImagePropertyOrientation` (1…8) to `UIImage.Orientation` (not the same as rawValue).
+    private static func uiImageOrientation(fromExifOrientation exif: Int) -> UIImage.Orientation {
+        switch exif {
+        case 2: return .upMirrored
+        case 3: return .down
+        case 4: return .downMirrored
+        case 5: return .leftMirrored
+        case 6: return .right
+        case 7: return .rightMirrored
+        case 8: return .left
+        default: return .up
+        }
     }
 
     static func pixelSize(from data: Data) -> (width: Int, height: Int)? {
